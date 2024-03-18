6f30750c7ee46d51531d9daf521cbbfdb7d5d984
Toñi <sanchezmaiquez84@gmail.com>
2024-03-18 08:26:40 +0100

    refactorizacion

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -2,13 +2,19 @@ package com.kreitek.editor;
 
 import com.kreitek.editor.edit.EditorFactory;
 import com.kreitek.editor.interfaces.ConsolePrint;
+import com.kreitek.editor.interfaces.DocumentStateManager;
 import com.kreitek.editor.interfaces.Editor;
 import com.kreitek.editor.utils.ConsolePrinterImpl;
+import com.kreitek.editor.utils.DocumentStateManagerImpl;
 
 public class Application {
 
     public static void main(String[] args) {
-        EditorFactory editorFactory = new EditorFactory();
+
+        ConsolePrint consolePrint = new ConsolePrinterImpl();
+        DocumentStateManager documentStateManager = new DocumentStateManagerImpl();
+
+        EditorFactory editorFactory = new EditorFactory(consolePrint, documentStateManager);
         Editor editor = editorFactory.getEditor();
         editor.run();
     }
--- editor/src/main/java/com/kreitek/editor/edit/EditorFactory.java
+++ editor/src/main/java/com/kreitek/editor/edit/EditorFactory.java
@@ -7,8 +7,14 @@ import com.kreitek.editor.utils.ConsolePrinterImpl;
 import com.kreitek.editor.utils.DocumentStateManagerImpl;
 
 public class EditorFactory {
-    private ConsolePrint consolePrint = new ConsolePrinterImpl();
-    private DocumentStateManager documentStateManager = new DocumentStateManagerImpl();
+    private ConsolePrint consolePrint;
+    private DocumentStateManager documentStateManager;
+
+    public EditorFactory(ConsolePrint consolePrint, DocumentStateManager documentStateManager) {
+        this.consolePrint = consolePrint;
+        this.documentStateManager = documentStateManager;
+    }
+
     public Editor getEditor() {
         return new ConsoleEditor(consolePrint, documentStateManager);
     }
